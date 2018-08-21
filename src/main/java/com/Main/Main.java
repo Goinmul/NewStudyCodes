d42a20fb877f5b1b19d1afae67c22df74568be98
Goinmul <21400045@handong.edu>
2018-08-21 13:21:56 +0900

    Added how to synchronize local eclipse project to remote git repos.

--- src/main/java/com/Main/Main.java
+++ src/main/java/com/Main/Main.java
@@ -4,7 +4,34 @@ import java.io.File;
 
 import com.ArrayList.ArrayListCode;
 import com.Exception.ExceptionCode;
+/**
+ * <starting a project connected to github>
 
+1] From baseline, make an empty repository in github and start coding there
+
+0. in github, create a new repository
+- open git bash (cmd)
+1. by using command 'cd' in git bash, move to a local directory that you want to download(clone) the remote git repository
+2. at the directory you chose, git clone the remote repository(in github)
+3. cd into the downloaded(cloned) local directory
+4. gradle init--type java-library
+- open eclipse
+5. import in eclipse - exisitng gradle project - import
+
+2] already have a working project in eclipse - connecting it on remote repository in git
+
+0. in github, create a new repository. Make sure not to create ANY FILES in the repository. Leave it empty.
+- open git bash (cmd)
+1. by using command 'cd' in git bash, move to a local directory where your working project is.
+2. at the directory you chose, git clone the remote repository(in github)
+3. on git bash, type belows :
+$ git remote add origin https://github.com/Goinmul/NewCodes.git
+$ git push -u origin master
+ * 
+ * 
+ * @author HyunWoo Kwon
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
