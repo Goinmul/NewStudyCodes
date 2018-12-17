e78e75801e0e236aa55ce48f4e9792642bc3b290
Goinmul <21400045@handong.edu>
2018-12-17 17:40:46 +0900

    #2 Finish debugging.

--- src/main/java/com/DataStructure/LinkedList.java
+++ src/main/java/com/DataStructure/LinkedList.java
@@ -6,10 +6,14 @@ public class LinkedList {
 	private Node head; // head index is null. practical node starts from head_next_node (index 0).
 	private int size;
 
+	public LinkedList() {
+		head = new Node(null);
+		size = 0;
+	}
 	// node form
 	private class Node{ // although it is class form, think of it as a CONSTRUCTOR in C.
 		private Object data; // data
-		private Node next = null; // link
+		private Node next; // link
 
 		// constructor(used when initialized with value)
 		Node (Object data){
@@ -31,30 +35,40 @@ public class LinkedList {
 		Node newNode = new Node(data);
 		Node node = getNode(this.size - 1); // since index is (size - 1), now 'node' is the last node
 		node.next = newNode; // newNode is added on the tail
+		size++;
 	}
 
 	// @param index : adding node after that index
-	public void add(Object data, int index) { // adding in the midst of the linked list. 
-		Node newNode = new Node(data);
-		Node node = getNode(index);
-		node.next = newNode;
+	public void add(Object data, int index) { // adding in the midst of the linked list.
+		if (size == 0) {
+			add_first(data);
+		}
+		else {
+			Node previous = getNode(index-1);
+			Node newNode = new Node(data);
+			previous.next = newNode;
+			newNode.next = previous.next.next;
+			size++;
+		}
 	}
 
 	public void delete(int index) {
 		if (index == 0 && size == 1) { // deleting next, when (head) -> (next) -> null.
 			head.next = null;
+			size--;
 		}
 		else {
 			Node previous = getNode(index-1);
 			//Node node_to_delete = getNode(index);
 			//node_to_delete.next = null;
 			previous.next = previous.next.next;
+			size--;
 		}
 	}
 
 	// (head)--index null, (head_next)--index 0
 	private Node getNode(int index) {
-		if (index < 0 || index >= size) {
+		if (index < 0 || index > size) {
 			throw new IndexOutOfBoundsException("size is " + size + ", input index : " + index);
 		}
 		Node node = head.next; // starting point.
@@ -68,13 +82,14 @@ public class LinkedList {
 	public Object getData(int index) {
 		return getNode(index).data;
 	}
-	
+
 	public void printNode() {
+		Node node = getNode(0);
 		for (int i =0; i < size; i++) {
-			Node node = getNode(0);
 			System.out.println("#" + i + " node data : " + node.data);
 			node = node.next;
 		}
+
 	}
 
 }
